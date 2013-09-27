354340940588fda6f4bb0bc4b4cc2575eb03b479
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2013-09-27 10:56:18 +0200

    Removes localhost from the .listen() method.

--- web.js
+++ web.js
@@ -2,4 +2,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(process.env.PORT || 5000, '127.0.0.1');
+}).listen(process.env.PORT || 5000);
